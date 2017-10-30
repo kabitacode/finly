3c810960ef3b235c8af50e817598eec88586843f
ciplex <masterkemseng@yahoo.com>
2017-10-30 08:38:07 +0700

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ gulp.task('useref', function(){
 gulp.task('images', function(){
   return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
   // Caching images that ran through imagemin
-  .pipe(cache(imagemin({
+  .pipe((imagemin({
       interlaced: true
     })))
   .pipe(gulp.dest('dist/images'))
